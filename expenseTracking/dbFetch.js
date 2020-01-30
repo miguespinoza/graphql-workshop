dc072c4ece0b59e2f934f316499a6057291e90bd
Miguel Espinoza <miguelangel.espinoza@honeywell.com>
2020-01-30 11:24:57 -0600

    improve fetch

--- expenseTracking/dbFetch.js
+++ expenseTracking/dbFetch.js
@@ -6,12 +6,17 @@ const API_URL = "http://13.85.67.250:5984/";
 function dbFetch(path, options) {
   return fetch(`${API_URL}${path}`, {
     headers: {
-      Authorization: `Basic ${base64.encode(`admin:admin`)}`,
+      //Authorization: `Basic ${base64.encode(`admin:admin`)}`,
       "content-type": "application/json"
     },
     ...options
-  }).then(r => r.json());
-}
+  }).then(r => {
+    if (r.ok) {
+      return r.json();
+    } else {
+      throw new Error(r.statusText);
+    }
+  });
 
 module.exports = {
   dbFetch
